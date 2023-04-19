313b7fdce9844689d771d62b7dba97ad93915db1
Dokyun Kim <donny5557@gmail.com>
2023-04-19 14:52:36 -0400

    Implemented profile picture and username

--- app.py
+++ app.py
@@ -18,7 +18,10 @@ def profile():
     """
     Renders profile page
     """
-    return render_template("profile.html")
+    profile_pic = "img/profile_picture.jpg"
+    return render_template(
+        "profile.html", username=username, photo=profile_pic, level=level
+    )
 
 
 @app.route("/edit-profile")
@@ -26,17 +29,31 @@ def edit_profile():
     """
     Renders page where user can change username, upload a profile picture
     """
-    pass
+    return render_template("editprofile.html")
 
 
-@app.route("/upload-successful")
+@app.route("/upload-successful", methods=["POST"])
 def photo_uploaded():
     """
     Uploads photo into system
     """
-    pass
+    uploaded_files = request.files["file"]
+    uploaded_files.save("static/img/profile_picture.jpg")
+    return redirect(url_for("profile"))
+
+
+@app.route("/username-changed", methods=["GET"])
+def username_changed():
+    """
+    Updates username to new username the user inputted
+    """
+    global username
+    username = request.args.get("new_username")
+    return redirect(url_for("profile"))
 
 
 if __name__ == "__main__":
     routines = []
+    username = "Grustler"
+    level = 1
     app.run(debug=True)
