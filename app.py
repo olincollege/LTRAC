a36bcdd555d7c2aee009242ba2a1122aac900437
Dokyun Kim <donny5557@gmail.com>
2023-04-20 16:21:31 -0400

    Implemented displaying Routines in `/plan`

--- app.py
+++ app.py
@@ -18,8 +18,13 @@ def plan_page():
     """
     Renders page for viewing routines/add new routine
     """
-    routines.append(plan.Routine())
-    return render_template("plan.html")
+
+    # print(routines)
+    length = len(routines)
+    my_list = ["1", "2", "3"]
+    return render_template(
+        "plan.html", routines=routines, length=length, my_list=my_list
+    )
 
 
 @app.route("/add-exercise")
@@ -36,7 +41,7 @@ def submit_exercise():
     Adds new exercise to routine
     Sends user back to add more exercises
     """
-    routines[-1].add_exercise("exercise-name", "sets")
+    # routines[-1].add_exercise("exercise-name", "sets")
     return redirect(url_for("add_new_exercise"))
 
 
@@ -45,9 +50,18 @@ def submit_routine():
     """
     Adds new routine to a list of routines
     """
+    # routines.append(plan.Routine())
     return redirect(url_for("plan_page"))
 
 
 if __name__ == "__main__":
-    routines = []
+    routines = [
+        {"Routine 1": ["Leg Press", "Squats", "Deadlift"]},
+        {"Routine 2": ["Lat pull down", "Pull ups"]},
+        {"Routine 3": ["Leg Press", "Squats"]},
+        {"Routine 4": ["Leg Press", "Squats"]},
+        {"Routine 5": ["Leg Press", "Squats"]},
+        {"Routine 6": ["Leg Press", "Squats"]},
+        {"Routine 7": ["Leg Press", "Squats"]},
+    ]
     app.run(debug=True)
