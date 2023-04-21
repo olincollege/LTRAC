a79afac23c612605f9baa7b34fdf3abe9ca3eb06
Ian <ianlum314@gmail.com>
2023-04-20 22:02:26 -0400

    write docstrings for routine

--- modules/plan.py
+++ modules/plan.py
@@ -11,11 +11,30 @@ class Exercise:
 
 
 class Routine:
+    """
+    An exercise routine storing user inputted exercises
+
+    Attributes:
+        exercises: A dictionary mapping the string of the exercise name to the
+            corresponding exercise object
+
+        name: A string representing the name of the routine
+    """
+
     def __init__(self, name_id):
         self.exercises = {}
         self.name = request.args.get(name_id)
 
     def add_exercise(self, name_id, sets_id):
+        """
+        Add an exercise to the routine
+
+        Args:
+            name_id: A string representing the html variable label for the name
+                of the exercise
+            sets_id: A string representing the html variable label for the
+                number of sets for the exercise
+        """
         exercise = Exercise(name_id, sets_id)
         self.exercises[exercise.name] = exercise
 
