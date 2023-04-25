31b3796ea58861c149e86ee70bdc9de41cba699d
Ian <ianlum314@gmail.com>
2023-04-25 14:54:06 -0400

    implement choosing workout days

--- modules/profile.py
+++ modules/profile.py
@@ -1,6 +1,17 @@
 """
 Functions for creating new users and viewing stats in LTRAC
 """
+from enum import Enum
+
+
+class Weekday(Enum):
+    MONDAY = 0
+    TUESDAY = 1
+    WEDNESDAY = 2
+    THURSDAY = 3
+    FRIDAY = 4
+    SATURDAY = 5
+    SUNDAY = 6
 
 
 class User:
@@ -21,6 +32,15 @@ class User:
         self.name = name
         self.xp_points = xp_points
         self.routines = {}
+        self.workout_days = {
+            Weekday.MONDAY: False,
+            Weekday.TUESDAY: False,
+            Weekday.WEDNESDAY: False,
+            Weekday.THURSDAY: False,
+            Weekday.FRIDAY: False,
+            Weekday.SATURDAY: False,
+            Weekday.SUNDAY: False,
+        }
 
     def level(self):
         """
@@ -36,3 +56,17 @@ class User:
             routine: A Routine object to be added to the user's routines
         """
         self.routines[routine.name] = routine
+
+    def set_workout_days(self, selected_days):
+        """
+        Set which days to workout
+
+        args:
+            selected_days: A list of Weekday objects, representing the days
+                to workout on
+        """
+        for day in self.workout_days:
+            if day in selected_days:
+                self.workout_days[day] = True
+            else:
+                self.workout_days[day] = False
