897a0129ee8f3a0879aebd136edbd7718430602f
Ian <ianlum314@gmail.com>
2023-04-25 13:54:05 -0400

    add xp and routines attribute, write docstring

--- modules/profile.py
+++ modules/profile.py
@@ -2,14 +2,20 @@
 Functions for creating new users and viewing stats in LTRAC
 """
 
-from flask import request
-
 
 class User:
     """
     A LTRAC user profile
+
+    Attributes:
+        name: A string representing the name of the user
+        xp_points: An integer representing the amount of experience points
+            the user has
+        routines: A dictionary of routine objects representing the user's
+            workout routines
     """
 
-    def __init__(self, name, level):
+    def __init__(self, name, xp_points=0):
         self.name = name
-        self.level = level
+        self.xp_points = xp_points
+        self.routines = {}
