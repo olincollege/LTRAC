8f88b114792f8ea0055201f04284f5f079beb411
Ian <ianlum314@gmail.com>
2023-04-27 14:46:42 -0400

    rename calendar module to dates, move Weekday to dates the name calendar interfered with the existing python library calendar

--- modules/profile.py
+++ modules/profile.py
@@ -1,17 +1,7 @@
 """
 Functions for creating new users and viewing stats in LTRAC
 """
-from enum import Enum
-
-
-class Weekday(Enum):
-    MONDAY = 0
-    TUESDAY = 1
-    WEDNESDAY = 2
-    THURSDAY = 3
-    FRIDAY = 4
-    SATURDAY = 5
-    SUNDAY = 6
+from dates import Weekday
 
 
 class User:
