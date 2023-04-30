d50c450ed5d7d08ebff28893837cb1ce93e69a38
Ian <ianlum314@gmail.com>
2023-04-30 16:51:39 -0400

    implement `test_level`

--- tests/test_user_internal.py
+++ tests/test_user_internal.py
@@ -34,3 +34,22 @@ def test_gain_xp(sample_user: User):
     xp_gain = 1000
     sample_user.gain_xp(xp_gain)
     assert sample_user.xp_points == xp_gain
+
+
+level_cases = [(0, 0), (1000, 1), (1001, 1), (999, 0)]
+
+
+@pytest.mark.parametrize("xp_gain,level", level_cases)
+def test_level(sample_user: User, xp_gain: int, level: int):
+    """
+    Test that User.level returns the correct level based on a variable ammount
+    of xp gained
+
+    Args:
+        sample_user: The User object to use
+        xp_gain: An integer representing how much xp the user will gain
+        level: An integer represetning the expected level based on the ammount
+            of xp gained
+    """
+    sample_user.gain_xp(xp_gain)
+    assert sample_user.level() == level
