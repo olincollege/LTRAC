c299785a7b9a07abe4b9f5f795865ba126c3bff1
Ian <ianlum314@gmail.com>
2023-04-30 10:10:29 -0400

    implement `test_to_json_correctness`

--- tests/test_user.py
+++ tests/test_user.py
@@ -4,6 +4,7 @@ Unit tests for User class
 
 import sys
 import os
+import json
 import shutil
 import pytest
 
@@ -55,3 +56,20 @@ def test_to_json_creates_path(sample_user: User):
         sample_user.to_json()
     except FileNotFoundError:
         assert False
+
+
+def test_to_json_correctness(sample_user: User):
+    """
+    Test that User.to_json creates a json file that matchs the expected file
+    structure
+
+    Args:
+        sample_user: The User object to use
+    """
+    sample_user.to_json()
+    with open(
+        "user_data/username/username.json", encoding="UTF-8"
+    ) as created_json, open(
+        "target_data/username.json", encoding="UTF-8"
+    ) as target_json:
+        assert json.load(created_json) == json.load(target_json)
