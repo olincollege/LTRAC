717c3185d4fb6d8f180387763bc0e3745b682445
Ian <ianlum314@gmail.com>
2023-04-30 16:36:10 -0400

    setup imports and `sample_user` fixture

--- tests/test_user_internal.py
+++ tests/test_user_internal.py
@@ -1,3 +1,23 @@
 """
 Unit tests for the User class internal methods
 """
+
+import sys
+import pytest
+
+
+sys.path.append("./")
+
+# pylint: disable=import-error, wrong-import-position
+from modules.profile import User
+
+
+@pytest.fixture
+def sample_user():
+    """
+    Create a sample user to be used for testing
+
+    Returns:
+        A user object with the name "username"
+    """
+    return User("username")
