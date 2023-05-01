4f608251f83ca6a6696f1e403cbda5aff30c8668
Ian <ianlum314@gmail.com>
2023-04-30 20:55:59 -0400

    implement to and from json

--- tests/test_routine.py
+++ tests/test_routine.py
@@ -4,6 +4,7 @@ Unit tests for Routine class
 
 import sys
 import os
+import json
 import pytest
 
 sys.path.append("./")
@@ -43,3 +44,35 @@ def sample_routine():
     routine.exercises["exercise1"].log_weights("2023-04-30", [1, 2])
     routine.exercises["exercise2"].log_weights("2023-04-30", [1, 2])
     return routine
+
+
+# pylint: disable=redefined-outer-name
+def test_to_json(sample_routine: Routine):
+    """
+    Test that Routine.to_json creates a json file that matches the expected
+    structure
+
+    Args:
+        sample_routine: The Routine object to use
+    """
+    sample_routine.to_json("user_data/routine1.json")
+    with open(
+        "user_data/routine1.json", "r", encoding="UTF-8"
+    ) as created_json, open(
+        "static_data/routines/routine1/routine1.json", "r", encoding="UTF-8"
+    ) as target_json:
+        assert json.load(created_json) == json.load(target_json)
+
+
+def test_from_json():
+    """
+    Test that Routine.from_json creates a Routine object with properly set
+    attributes
+    """
+    routine = Routine("routine1")
+    routine.add_exercise(Exercise("exercise1", 2))
+    routine.add_exercise(Exercise("exercise2", 2))
+    assert (
+        Routine.from_json("static_data/routines/routine1/routine1.json")
+        == routine
+    )
